900766a905d2cea47ada2a82de4df81d83c42b96
Aravindan007 <53030761+Aravindan007@users.noreply.github.com>
2019-07-25 10:01:01 +0530

    Add files via upload

---
+++ dominator.py
@@ -0,0 +1,11 @@
+from collections import Counter
+l=[]
+n=int(input(""))
+for i in range(0,n):
+    a=int(input(""))
+    l.append(a)
+print (dict(Counter(l)))
+b=len(l)/2
+print(int(b))
+if (kv:b<a):
+    print(max(a))
